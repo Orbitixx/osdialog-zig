8b718c261e5b31a7459d6968eb221f51751feab9
obx0 <mark@orbitixx.com>
2025-11-01 19:10:47 -0400

    build.zig tweaks

--- build.zig
+++ build.zig
@@ -1,17 +1,11 @@
 const std = @import("std");
 const builtin = @import("builtin");
 
-// Although this function looks imperative, it does not perform the build
-// directly and instead it mutates the build graph (`b`) that will be then
-// executed by an external runner. The functions in `std.Build` implement a DSL
-// for defining build steps and express dependencies between them, allowing the
-// build runner to parallelize the build automatically (and the cache system to
-// know when a step doesn't need to be re-run).
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const module = b.addModule("osdialog-zig", .{
+    const lib_mod = b.addModule("osdialog-zig", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -20,7 +14,7 @@ pub fn build(b: *std.Build) void {
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "osdialog-zig",
-        .root_module = module,
+        .root_module = lib_mod,
     });
 
     lib.addCSourceFile(.{ .file = b.path("src/osdialog/osdialog.c") });
@@ -41,16 +35,20 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
+    const module = b.addModule("osdialog-zig", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    module.linkLibrary(lib);
+
     const lib_unit_tests = b.addTest(.{
         .root_module = module,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // Exposes a `test` step to the `zig build --help` menu, providing a way
-    // for the user to request running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
 }
