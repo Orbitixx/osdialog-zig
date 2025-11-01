297e83af405be08d8f84d7dfa5e6a8388ff7682d
obx0 <mark@orbitixx.com>
2025-11-01 18:34:51 -0400

    Build module export tweak

--- build.zig
+++ build.zig
@@ -54,6 +54,14 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
+    const module = b.addModule("osdialog-zig", .{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    module.linkLibrary(lib);
+
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
