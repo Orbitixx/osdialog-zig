ba897aee81d891ff5da26cc61fd8bd8eb0a2b0e8
obx0 <mark@orbitixx.com>
2025-11-01 19:24:35 -0400

    build.zig changes

--- build.zig
+++ build.zig
@@ -5,14 +5,17 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    // Create the module first
     const lib_mod = b.addModule("osdialog-zig", .{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
+    // Set include path on the module
     lib_mod.addIncludePath(b.path("src/osdialog/"));
 
+    // Create the library using that module
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "osdialog-zig",
@@ -37,16 +40,9 @@ pub fn build(b: *std.Build) void {
 
     b.installArtifact(lib);
 
-    const module = b.addModule("osdialog-zig", .{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    module.linkLibrary(lib);
-
+    // Tests
     const lib_unit_tests = b.addTest(.{
-        .root_module = module,
+        .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
